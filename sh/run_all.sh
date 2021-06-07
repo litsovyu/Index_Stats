7fcfc93e35771a84e8ddcdc026a1f766c3fd1ae1
litsovyu <82654310+litsovyu@users.noreply.github.com>
2021-06-07 17:08:23 +0300

    Update run_all.sh

--- sh/run_all.sh
+++ sh/run_all.sh
@@ -6,3 +6,5 @@ sh -c  /opt/mssql-tools/bin/sqlcmd  -S ${DB_HOST} -U ${DB_USER} -P ${DB_PASS}   
 
 sh -c  /opt/mssql-tools/bin/sqlcmd  -S ${DB_HOST} -U ${DB_USER} -P ${DB_PASS}   -i "./sql/Index_Stats.sql" 
 
+# or
+#sh -c  /opt/mssql-tools/bin/sqlcmd  -S ${DB_HOST} -U ${DB_USER} -P ${DB_PASS} -Q "EXECUTE dbo.IndexOptimize @Databases = ${DB_NAME},  @FragmentationLow = NULL, @FragmentationMedium = 'INDEX_REORGANIZE,INDEX_REBUILD_ONLINE,INDEX_REBUILD_OFFLINE', @FragmentationHigh = 'INDEX_REBUILD_ONLINE,INDEX_REBUILD_OFFLINE', @FragmentationLevel1 = 5, @FragmentationLevel2 = 30, @UpdateStatistics = 'ALL', @OnlyModifiedStatistics = 'Y'"
