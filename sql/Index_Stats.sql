30c1ac582a3f13735ac39fd8646adef552a2176f
litsovyu <82654310+litsovyu@users.noreply.github.com>
2021-07-09 14:58:14 +0300

    Delete Index_Stats.sql

--- sql/Index_Stats.sql
+++
@@ -1,9 +0,0 @@
-EXECUTE 	
-@Databases = "$DB_NAME",  --не факт что это сработает
-@FragmentationLow = NULL,
-@FragmentationMedium = 'INDEX_REORGANIZE,INDEX_REBUILD_ONLINE,INDEX_REBUILD_OFFLINE',
-@FragmentationHigh = 'INDEX_REBUILD_ONLINE,INDEX_REBUILD_OFFLINE',
-@FragmentationLevel1 = 5,
-@FragmentationLevel2 = 30,
-@UpdateStatistics = 'ALL',
-@OnlyModifiedStatistics = 'Y'
